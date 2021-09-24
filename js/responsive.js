e577f15d5122cb661743fb65edbfefa01624755e
mizuno-shoji <shsfshoji@gmail.com>
2021-09-24 16:18:52 +0900

    モバイルレイアウト

---
+++ js/responsive.js
@@ -0,0 +1,19 @@
+$(function(){
+  $('#mobile-menu-btn').on('click', function() {
+    $('.header-menu').css({
+      right: '0',
+      transition: 'right 0.3s',
+    })
+    $('#mobile-menu-btn').hide();
+    $('#mobile-menu-close-btn').show();
+  });
+
+  $('#mobile-menu-close-btn').on('click', function() {
+    $('.header-menu').css({
+      right: '-320px',
+      transition: 'right 0.3s',
+    })
+    $('#mobile-menu-close-btn').hide();
+    $('#mobile-menu-btn').show();
+  })
+});

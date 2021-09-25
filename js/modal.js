a8ca517ce9278340e31221573fff35808c4dc0f6
mizuno-shoji <shsfshoji@gmail.com>
2021-09-26 02:43:04 +0900

    Aboutコンテンツの作成

--- js/modal.js
+++ js/modal.js
@@ -56,17 +56,31 @@ $(function() {
         id: 'modal-contents-about',
         hidden: 'hidden',
       })
-      .append($('<h1>').text('relativezについて'))
-      .append($('<h2>').text('見出し2'))
-      .append($('<p>').text(
-        'コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。' +
-        'コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。'
+      .append($('<h1>').text('About'))
+      .append($('<p>').text('relativezは、文献の引用ネットワークを可視化するWebアプリケーションです。'))
+      .append($('<p>').text('目的は下記の3点を達成することです。'))
+      .append($('<ul>')
+        .append($('<li>').text('文献同士の関係を視覚化することで文脈が把握しやすくなること'))
+        .append($('<li>').text('情報量が増えたときに全体をイメージしやすくすること'))
+        .append($('<li>').text('研究に必要な文献情報を整理できること'))
+      )
+      .append($('<p>')
+        .text('relativezが想像する理想的なシーンは、複数の独立したアーティストが集まる場所において、個々人の持つ問題意識について、'+
+              'お互いがお互いにその複雑さを理解しつつ共通点を見出せる環境です。このWebアプリケーションが会話や勉強会など様々な場面で'+
+              '制作の一助になることを願っています。'))
+      .append($('<h1>').text('relativezプロジェクトへの貢献方法'))
+      .append($('<h2>').text('データ提供者として貢献する'))
+      .append($('<p>')
+        .text(
+          'データソースを作成して提供していただくと、relativezにアクセスした他のユーザーが閲覧可能なネットワークグラフとして表示します。'+
+          '「データソース選択」から提供していただいたデータが選択可能になります。'
         )
       )
-      .append($('<h2>').text('見出し2'))
-      .append($('<p>').text(
-        'コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。' +
-        'コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。コンテンツです。'
+      .append($('<h2>').text('開発者として貢献する'))
+      .append($('<p>')
+        .text(
+          'このWebアプリはMITライセンスを適用しており、自由に複製・配布・修正されることを無制限に許可したオープンソースプロジェクトです。'+
+          'ご興味のある方はGitHubでぜひご参加願います。'
         )
       )
     );
@@ -88,10 +102,9 @@ $(function() {
           id: 'data-selection',
           name: 'data-selection',
         })
-        .append($('<option>').attr('value', 'initial_graph').text('日本の美術'))
+        .append($('<option>').attr('value', 'initial_graph').text('日本美術の制度論系 (データ作成者：大原由)'))
         .append($('<option>').attr('value', 'sample_1').text('サンプル1'))
         .append($('<option>').attr('value', 'sample_2').text('サンプル2'))
-        .append($('<option>').attr('value', 'sample_3').text('サンプル3'))
       )
       .append($('<button>').addClass('button-component').attr('id', 'show-selected-graph-btn').text('ネットワークグラフ表示'))
     );
